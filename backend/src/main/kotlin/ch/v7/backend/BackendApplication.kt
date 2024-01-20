c2a9d5d29a9eb9e61d2bba53fe3c97cf2e579b01
nilsrothe <nils.rothe@healthinal.com>
2024-01-20 17:08:41 +0100

    feat(V7WE-22): a lot of backend stuff

--- backend/src/main/kotlin/ch/v7/backend/BackendApplication.kt
+++ backend/src/main/kotlin/ch/v7/backend/BackendApplication.kt
@@ -1,5 +1,8 @@
 package ch.v7.backend
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
@@ -8,8 +11,11 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @EnableScheduling
+@OpenAPIDefinition(servers = [Server(url = "/api")], info = Info(title = "V7 API", version = "v0"))
 class BackendApplication
 
 fun main(args: Array<String>) {
-	runApplication<BackendApplication>(*args)
+    runApplication<BackendApplication>(args = args) {
+        beans.forEach(::addInitializers)
+    }
 }
---
+++ backend/src/main/resources/db/seeds/user.sql
@@ -0,0 +1,2 @@
+insert into t_user (id, first_name, last_name, phone, e_mail, user_role, password)
+values ('c3bc1203-ec7f-428d-a4aa-01c0c3b40abd', 'nils', 'rothe', '0773434334', 'nilsrothe05@gmail.com', 'ADMIN', 'test' );

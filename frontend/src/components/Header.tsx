d65305c0022d00ac4fc79c72a53bdc71ad76a5bb
nilsrothe <nils.rothe@healthinal.com>
2024-01-24 21:04:51 +0100

    feat(V7WE-8): Website under construction message

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,8 +1,8 @@
-import { AppBar, Box, Drawer, IconButton, styled, Toolbar, Typography } from '@mui/material';
 import { mdiMenu } from '@mdi/js';
-import { Link } from './Link';
-import { useState } from 'react';
 import Icon from '@mdi/react';
+import { AppBar, Box, Drawer, IconButton, styled, Toolbar, Typography } from '@mui/material';
+import { useState } from 'react';
+import { Link } from './Link';
 
 const StyledLink = styled(Link)`
   padding: 0.35rem 0.7rem;
@@ -19,78 +19,74 @@ const StyledDrawerLink = styled(Link)`
 `;
 
 export default function Header() {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const links = [
-        { to: '/', text: 'Start Seite' },
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const links = [{ to: '/', text: 'Start Seite' }];
 
-    ];
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen((prevState) => !prevState);
+  };
 
-    const handleDrawerToggle = () => {
-        setIsDrawerOpen((prevState) => !prevState);
-    };
-
-    return (
-        <>
-            <AppBar sx={headerStyle} elevation={0}>
-                <Toolbar>
-
-                    <Typography style={headerTitleStyle}>Dashboard-Control</Typography>
-                    <IconButton
-                        color={'primary'}
-                        aria-label={'open drawer'}
-                        edge={'end'}
-                        onClick={handleDrawerToggle}
-                        sx={{ display: { sm: 'none' }, marginLeft: 'auto', marginRight: '10px' }}>
-                        <Icon path={mdiMenu} size={1.4} />
-                    </IconButton>
-                    <Box sx={linkGrid}>
-                        {links.map((link, index) => (
-                            <StyledLink key={index} to={link.to}>
-                                <Typography>{link.text}</Typography>
-                            </StyledLink>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                variant="temporary"
-                open={isDrawerOpen}
-                onClose={handleDrawerToggle}
-                ModalProps={{
-                    keepMounted: true,
-                }}
-                sx={{
-                    display: { xs: 'block', sm: 'none' },
-                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: '50%' },
-                }}>
-                <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', backgroundColor: '#eef2f2', height: '100%' }}>
-                    {links.map((link, index) => (
-                        <StyledDrawerLink key={index} to={link.to}>
-                            <Typography>{link.text}</Typography>
-                        </StyledDrawerLink>
-                    ))}
-                </Box>
-            </Drawer>
-        </>
-    );
+  return (
+    <>
+      <AppBar sx={headerStyle} elevation={0}>
+        <Toolbar>
+          <Typography style={headerTitleStyle}>V7</Typography>
+          <IconButton
+            color={'primary'}
+            aria-label={'open drawer'}
+            edge={'end'}
+            onClick={handleDrawerToggle}
+            sx={{ display: { sm: 'none' }, marginLeft: 'auto', marginRight: '10px' }}>
+            <Icon path={mdiMenu} size={1.4} />
+          </IconButton>
+          <Box sx={linkGrid}>
+            {links.map((link, index) => (
+              <StyledLink key={index} to={link.to}>
+                <Typography>{link.text}</Typography>
+              </StyledLink>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="temporary"
+        open={isDrawerOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: '50%' },
+        }}>
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', backgroundColor: '#eef2f2', height: '100%' }}>
+          {links.map((link, index) => (
+            <StyledDrawerLink key={index} to={link.to}>
+              <Typography>{link.text}</Typography>
+            </StyledDrawerLink>
+          ))}
+        </Box>
+      </Drawer>
+    </>
+  );
 }
 
 const headerStyle = {
-    backgroundColor: '#86C4FD',
-    top: '2rem',
-    borderRadius: 2,
-    position: 'sticky' as const,
+  backgroundColor: '#86C4FD',
+  top: '2rem',
+  borderRadius: 2,
+  position: 'sticky' as const,
 };
 
 const linkGrid = {
-    display: { xs: 'none', sm: 'flex' },
-    marginLeft: 'auto',
-    gap: '20px',
-    flexWrap: 'wrap',
+  display: { xs: 'none', sm: 'flex' },
+  marginLeft: 'auto',
+  gap: '20px',
+  flexWrap: 'wrap',
 };
 
 const headerTitleStyle = {
-    fontSize: '18px',
-    color: '#575756',
-    marginLeft: '10px',
+  fontSize: '18px',
+  color: '#575756',
+  marginLeft: '10px',
 };
---
+++ frontend/src/components/WebsiteUnderConstruction.tsx
@@ -0,0 +1,8 @@
+export default function WebsiteUnderConstruction() {
+  return (
+    <div>
+      <h1>Herzlich Willkommen beim V7 in Bauma</h1>
+      <h2>🏗️ Website under construction.... Comming soon 🏗️</h2>
+    </div>
+  );
+}

99682d2d30fbf7ccc4426dfb0e37e3f9012ca50f
nilsrothe <nils.rothe@healthinal.com>
2024-01-24 22:12:52 +0100

    feat(V7WE-16): added sonarcloud and IntegrationTest

--- frontend/src/components/Link.tsx
+++ frontend/src/components/Link.tsx
@@ -1,6 +1,6 @@
-import { Link as MuiLink, LinkProps } from '@mui/material';
+import { LinkProps, Link as MuiLink } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 export function Link(props: LinkProps<typeof RouterLink>) {
-    return <MuiLink component={RouterLink} {...props} />;
+  return <MuiLink component={RouterLink} {...props} />;
 }

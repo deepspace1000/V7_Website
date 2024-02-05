d52d07bf134377241e9eab41b1cea9586e0f3582
nilsrothe <nils.rothe@healthinal.com>
2024-02-05 23:09:06 +0100

    feat(V7WE-39): removing token automatically in frontend when invalid

--- frontend/src/state/api/interceptors.ts
+++ frontend/src/state/api/interceptors.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401 && error.response.data === 'Invalid Token') {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+    }
+
+    return error;
+  },
+);
+
 axios.interceptors.request.use((request) => {
   const token = localStorage.getItem('token');
   if (token) {
